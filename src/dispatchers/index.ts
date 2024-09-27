6129c51ec4b275e97c3e237d427549852f7281ff
Schmock <rsk.rook@gmail.com>
2024-09-27 19:43:28 +0200

    fixes

--- src/dispatchers/index.ts
+++ src/dispatchers/index.ts
@@ -1,12 +1,14 @@
+
 import { ICommand } from '../commands';
 import { IEvent } from '../events';
 import { Store } from 'redux';
+
 // Command Dispatcher
 export class CommandDispatcher {
-  private commandHandlers: Map<string, (command: ICommand) => Promise<void>> = new Map();
+  private commandHandlers: Map<string, (command: ICommand) => void> = new Map();
 
-  registerHandler<T extends ICommandHandler<ICommand>>(handler: T) : void {
-    this.commandHandlers.set((handler as any).constructor.name.replace(/([a-z])([A-Z])/g, '$1_$2').toUpperCase().split('_').slice(0, (handler as any).constructor.name.replace(/([a-z])([A-Z])/g, '$1_$2').split('_').indexOf('Command')+1).join('_'), handler.handle as (command: ICommand) => Promise<void>);
+  registerHandler<T extends ICommandHandler<ICommand>>(handler: T): void {
+    this.commandHandlers.set((handler as any).constructor.name.replace(/([a-z])([A-Z])/g, '$1_$2').toUpperCase().split('_').slice(0, (handler as any).constructor.name.replace(/([a-z])([A-Z])/g, '$1_$2').split('_').indexOf('Command') + 1).join('_'), handler.handle as (command: ICommand) => void);
   }
 
   dispatch(command: ICommand): void {
@@ -14,7 +16,7 @@ export class CommandDispatcher {
     if (handler) {
       handler(command);
     } else {
-      throw new Error(`No handler registered for command type: ${command.type}`);
+      throw new Error(\`No handler registered for command type: \${command.type}\`);
     }
   }
 }
@@ -24,32 +26,35 @@ export class EventDispatcher {
   private eventHandlers: Map<string, (event: IEvent) => void> = new Map();
 
   registerHandler<T extends IEventHandler<IEvent>>(handler: T): void {
-    this.eventHandlers.set((handler as any).constructor.name.replace(/([a-z])([A-Z])/g, '$1_$2').toUpperCase().split('_').slice(0, (handler as any).constructor.name.replace(/([a-z])([A-Z])/g, '$1_$2').split('_').indexOf('Event')+1).join('_'), handler.handle as (command: ICommand) => Promise<void>);
+    this.eventHandlers.set((handler as any).constructor.name.replace(/([a-z])([A-Z])/g, '$1_$2').toUpperCase().split('_').slice(0, (handler as any).constructor.name.replace(/([a-z])([A-Z])/g, '$1_$2').split('_').indexOf('Event') + 1).join('_'), handler.handle as (event: IEvent) => void);
   }
 
   dispatch(event: IEvent): void {
     const handler = this.eventHandlers.get(event.type);
     if (handler) {
-      handler(event);
+      try {
+        handler(event);
+      } catch (error) {
+        console.error(`Error in event handler:`, error);
+      }
     } else {
-      throw new Error(`No handler registered for event type: ${event.type}`);
+      throw new Error(\`No handler registered for event type: \${event.type}\`);
     }
   }
 }
 
-export interface ICommandHandler<T extends ICommand>{
-  handle(command: ICommand): Promise<any | void> | void;
+export interface ICommandHandler<T extends ICommand> {
+  handle(command: ICommand): void;
 }
 
 export interface IEventHandler<T extends IEvent> {
-  handle(action: IEvent): Promise<any | void> | void;
+  handle(event: IEvent): void;
 }
 
 export abstract class CommandHandler<T extends ICommand> implements ICommandHandler<T> {
-  abstract handle(command: ICommand): Promise<any | void> | void;
+  abstract handle(command: ICommand): void;
 }
 
 export abstract class EventHandler<T extends IEvent> implements IEventHandler<T> {
-  abstract handle(event: IEvent): Promise<any | void> | void;
+  abstract handle(event: IEvent): void;
 }
-
